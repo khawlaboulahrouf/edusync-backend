bc38865a938515c18355fae637268ad8108daf82
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-04-22 17:11:26 +0100

    modifie le design de formulaire login avec tailwind css

--- public/login.php
+++ public/login.php
@@ -1,26 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
     <title>Login</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
 
-<?php
-if(isset($_GET['error'])){
-    echo "<p style='color:red'>Email ou mot de passe incorrect</p>";
-}
-?>
+<body class="bg-gray-100">
 
-<form action="../scripts/authprocess.php" method="POST">
+<div class="flex items-center justify-center min-h-screen">
 
-    <input type="hidden" name="action" value="login">
+    <div class="w-full max-w-md bg-white p-6 rounded-lg shadow">
 
-    <input type="text" name="email" placeholder="Email"><br><br>
-    <input type="password" name="password" placeholder="Password"><br><br>
+        <h2 class="text-2xl font-bold text-center mb-6">Connexion</h2>
 
-    <button type="submit">Login</button>
+        <!-- message error -->
+        <?php
+        if(isset($_GET['error'])){
+            echo "<p style='color:red;'>Email ou mot de passe incorrect</p>";
+        }
+        ?>
 
-</form>
+        <form action="../scripts/authprocess.php" method="POST">
+
+            <input type="hidden" name="action" value="login">
+
+            <div class="mb-4">
+                <label>Email</label>
+                <input type="text" name="email" class="w-full p-2 border rounded">
+            </div>
+
+            <div class="mb-4">
+                <label>Mot de passe</label>
+                <input type="password" name="password" class="w-full p-2 border rounded">
+            </div>
+
+            <button type="submit" class="w-full bg-blue-600 text-white p-2 rounded">
+                Se connecter
+            </button>
+
+        </form>
+
+    </div>
+
+</div>
 
 </body>
 </html>

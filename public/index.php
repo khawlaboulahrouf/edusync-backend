5df0f8fb73ce12817e69589ec44346a3f6a118f0
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-04-22 17:00:38 +0100

    remplire le fichier indexpour choisir la page que vous voulez

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EduSync</title>
+</head>
+<body>
+
+<h2>Bienvenue dans EduSync</h2>
+
+<a href="login.php">Login</a><br><br>
+<a href="register.php">Register</a>
+
+</body>
+</html>
